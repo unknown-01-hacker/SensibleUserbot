f4a1a7a56eb7500bf89d144405c6081335a3a086
spandey112 <official.shashwat.pandey@gmail.com>
2020-08-06 16:15:36 +0530

    Fixing GBAN

--- userbot/plugins/globalban.py
+++ userbot/plugins/globalban.py
@@ -60,17 +60,16 @@ from userbot import bot, bot
 
 @bot.on(admin_cmd(pattern=f"gban(?: |$)(.*)", allow_sudo=True))
 @command(outgoing=True, pattern="^.gban(?: |$)(.*)")
-async def gspider(rk): 
-   lazy = rk ; sender = await lazy.get_sender() ; me = await lazy.bot.get_me()
+sender = await get_sender() ; me = await bot.get_me()
    if not sender.id == me.id:
-        rkp = await lazy.reply("`processing...`")
+        bot = await reply("`processing...`")
    else:
-    	rkp = await lazy.edit("`processing...`")      
-   me = await rk.bot.get_me() ; await rkp.edit(f"`{DEFAULTUSER}:` **Requesting  to gban user!**") ; my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id) ; my_username = f"@{me.username}" if me.username else my_mention ; chat = await rk.get_chat() ; a = b = 0
-   if rk.is_private:       
-   	user = rk.chat ; reason = rk.pattern_match.group(1) ; chat_title = 'PM'  
+    	bot = await edit("`processing...`")      
+   me = await bot.get_me() ; await bot.edit(f"`{DEFAULTUSER}:` **Requesting  to gban user!**") ; my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id) ; my_username = f"@{me.username}" if me.username else my_mention ; chat = await get_chat() ; a = b = 0
+   if is_private:       
+   	user = chat ; reason = pattern_match.group(1) ; chat_title = 'PM'  
    else:
-   	chat_title = rk.chat.title  
+   	chat_title = chat.title  
    try:       
     user, reason = await get_user_from_event(rk)  
    except:
@@ -79,35 +78,35 @@ async def gspider(rk):
      if not reason:
        reason = 'Private'
    except:
-   	return await rkp.edit(f"`{DEFAULTUSER}:`**Error! Unknown user.**")
+   	return await bot.edit(f"`{DEFAULTUSER}:`**Error! Unknown user.**")
    if user:      
         if user.id == 709723121:     
-    	             return await rkp.edit(f"`{DEFAULTUSER}:`**Error! cant gban this user.**")
+    	             return await bot.edit(f"`{DEFAULTUSER}:`**Error! cant gban this user.**")
         try:
           from userbot.modules.sql_helper.gmute_sql import gmute            
         except:
    	     pass
         try:
-          await rk.bot(BlockRequest(user))
+          await bot(BlockRequest(user))
           block = 'True'
         except:      
            pass
-        testrk = [d.entity.id for d in await rk.bot.get_dialogs() if (d.is_group or d.is_channel) ]                          
+        testrk = [d.entity.id for d in await bot.get_dialogs() if (d.is_group or d.is_channel) ]                          
         for i in testrk:
             try:
-                 await rk.bot.edit_permissions(i, user, view_messages=False)          
+                 await bot.edit_permissions(i, user, view_messages=False)          
                  a += 1
-                 await rkp.edit(f"`{DEFAULTUSER}:` **Requesting  to gban user!\nGbanned {a} chats.....**")
+                 await bot.edit(f"`{DEFAULTUSER}:` **Requesting  to gban user!\nGbanned {a} chats.....**")
             except:
                  b += 1                     
    else:
-       await rkp.edit(f"`{DEFAULTUSER}:` **Reply to a user !! **")        
+       await bot.edit(f"`{DEFAULTUSER}:` **Reply to a user !! **")        
    try:
      if gmute(user.id) is False:
-            return await rkp.edit(f"`{DEFAULTUSER}:`**Error! User probably already gbanned.**")
+            return await bot.edit(f"`{DEFAULTUSER}:`**Error! User probably already gbanned.**")
    except:
     	pass
-   return await rkp.edit(f"`{DEFAULTUSER}:` **Gbanned [{user.first_name}](tg://user?id={user.id}) in {a} chat(s) , Blocked user and added to Gban watch **") 
+   return await bot.edit(f"`{DEFAULTUSER}:` **Gbanned [{user.first_name}](tg://user?id={user.id}) in {a} chat(s) , Blocked user and added to Gban watch **") 
         
 
 
@@ -115,16 +114,16 @@ async def gspider(rk):
 @bot.on(admin_cmd(pattern=f"ungban(?: |$)(.*)", allow_sudo=True))
 @command(outgoing=True, pattern="^.ungban(?: |$)(.*)")
 async def gspider(rk):
-   lazy = rk ; sender = await lazy.get_sender() ; me = await lazy.bot.get_me()
+   sender = await get_sender() ; me = await bot.get_me()
    if not sender.id == me.id:
-        rkp = await lazy.reply("`processing...`")
+        bot = await reply("`processing...`")
    else:
-    	rkp = await lazy.edit("`processing...`")   
-   me = await rk.bot.get_me() ; await rkp.edit(f"`{DEFAULTUSER}:` **Requesting  to ungban user!**") ; my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id) ; my_username = f"@{me.username}" if me.username else my_mention ; chat = await rk.get_chat() ; a = b = 0
-   if rk.is_private:       
-   	user = rk.chat ; reason = rk.pattern_match.group(1) ; chat_title = 'PM'  
+    	bot = await edit("`processing...`")   
+   me = await bot.get_me() ; await bot.edit(f"`{DEFAULTUSER}:` **Requesting  to ungban user!**") ; my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id) ; my_username = f"@{me.username}" if me.username else my_mention ; chat = await get_chat() ; a = b = 0
+   if is_private:       
+   	user = chat ; reason = pattern_match.group(1) ; chat_title = 'PM'  
    else:
-   	chat_title = rk.chat.title  
+   	chat_title = chat.title  
    try:       
     user, reason = await get_user_from_event(rk)  
    except:
@@ -133,33 +132,33 @@ async def gspider(rk):
      if not reason:
        reason = 'Private'
    except:
-   	return await rkp.edit(f"`{DEFAULTUSER}:`**Error! Unknown user.**")
+   	return await bot.edit(f"`{DEFAULTUSER}:`**Error! Unknown user.**")
    if user:      
         if user.id == 709723121:     
-    	             return await rkp.edit(f"`{DEFAULTUSER}:`**Error! cant ungban this user.**")
+    	             return await bot.edit(f"`{DEFAULTUSER}:`**Error! cant ungban this user.**")
         try:
           from userbot.modules.sql_helper.gmute_sql import ungmute
         except:
    	     pass
         try:
-          await rk.bot(UnblockRequest(user))
+          await bot(UnblockRequest(user))
           block = 'True'
         except:      
            pass
-        testrk = [d.entity.id for d in await rk.bot.get_dialogs() if (d.is_group or d.is_channel) ]                          
+        testrk = [d.entity.id for d in await bot.get_dialogs() if (d.is_group or d.is_channel) ]                          
         for i in testrk:
             try:
-                 await rk.bot.edit_permissions(i, user, send_messages=True)          
+                 await bot.edit_permissions(i, user, send_messages=True)          
                  a += 1
-                 await rkp.edit(f"`{DEFAULTUSER}:` **Requesting  to ungban user!\nunGbanned {a} chats.....**")
+                 await bot.edit(f"`{DEFAULTUSER}:` **Requesting  to ungban user!\nunGbanned {a} chats.....**")
             except:
                  b += 1                     
    else:
-       await rkp.edit(f"`{DEFAULTUSER}:` **Reply to a user !! **")        
+       await bot.edit(f"`{DEFAULTUSER}:` **Reply to a user !! **")        
    try:
      if ungmute(user.id) is False:
-            return await rkp.edit(f"`{DEFAULTUSER}:`**Error! User probably already ungbanned.**")
+            return await bot.edit(f"`{DEFAULTUSER}:`**Error! User probably already ungbanned.**")
    except:
     	pass
-   return await rkp.edit(f"`{DEFAULTUSER}:` **UnGbanned [{user.first_name}](tg://user?id={user.id}) in {a} chat(s) , UnBlocked and removed user from Gban watch **") 
+   return await bot.edit(f"`{DEFAULTUSER}:` **UnGbanned [{user.first_name}](tg://user?id={user.id}) in {a} chat(s) , UnBlocked and removed user from Gban watch **") 
         
