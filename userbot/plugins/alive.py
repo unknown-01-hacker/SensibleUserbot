cbfc877720d93fb9e665f759994f96ed7762c39d
Ceo White Hat Cracks <60309463+spandey112@users.noreply.github.com>
2020-08-23 14:21:13 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -29,9 +29,9 @@ async def amireallyalive(alive):
     await alive.delete()
     await borg.send_file(alive.chat_id, ALIVE_IMG,caption=ALIVE_caption)
 
-
-    @borg.on(admin_cmd(pattern=r"Alive", allow_sudo=True))
-    async def friday(alive):
+@borg.on(admin_cmd(pattern=r"Alive", allow_sudo=True))
+async def friday(alive):
     chat = await alive.get_chat()
     """ For .alive command, check if the bot is running.  """
     await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+
