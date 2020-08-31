22d516bf9bada702ff3f8c50855d618047e485fc
Ceo White Hat Cracks <60309463+spandey112@users.noreply.github.com>
2020-08-31 16:32:38 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,7 +9,7 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-ALIVE_IMG = "https://telegra.ph/file/6225d447ed8ba1d23079f.jpg"
+ALIVE_IMG = "https://telegra.ph/file/7f0046214f2331d691679.jpg"
 ALIVE_caption = "`Sensible Userbot IS:` **ONLINE**\n\n"
 ALIVE_caption += "**SYSTEM STATUS**\n\n"
 ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
@@ -19,7 +19,7 @@ ALIVE_caption += "**Sensible  OS** : `3.14`\n\n"
 ALIVE_caption += "**Current Sat** : `Sensible Userbot Sat-2.95`\n\n"
 ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
 ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-ALIVE_caption += "**Bot Made By @ceowhitehatcracks \n\n"
+ALIVE_caption += "**Bot Made By @ceowhitehatcracks** \n\n"
 ALIVE_caption += "Copyright By [CEOWHITEHATCRACKS](GitHub.com/spandey112)\n\n"
 ALIVE_caption += "[Deploy SensibleUserbot](GitHub.com/spandey112/SensibleUserbot)"
 #@command(outgoing=True, pattern="^.alive$")
