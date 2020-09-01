177094f71462bf42ce9678a1090d47dbe3062752
spandey112 <official.shashwat.pandey@gmail.com>
2020-09-01 09:56:05 +0530

    Update notes_sql.py

--- userbot/plugins/sql_helper/notes_sql.py
+++ userbot/plugins/sql_helper/notes_sql.py
@@ -1,50 +1,67 @@
-try:
-    from userbot.modules.sql_helper import SESSION, BASE
-except ImportError:
-    raise Exception("Hello!")
-from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
+from sqlalchemy import Column, UnicodeText, LargeBinary, Numeric
+from userbot.plugins.sql_helper import SESSION, BASE
 
 
-class Notes(BASE):
+class notes(BASE):
     __tablename__ = "notes"
-    chat_id = Column(String(14), primary_key=True)
-    keyword = Column(UnicodeText, primary_key=True, nullable=False)
-    reply = Column(UnicodeText, nullable=False)
+    note = Column(UnicodeText, primary_key=True)
+    reply = Column(UnicodeText)
+    note_type = Column(Numeric)
+    media_id = Column(UnicodeText)
+    media_access_hash = Column(UnicodeText)
+    media_file_reference = Column(LargeBinary)
 
-    def __init__(self, chat_id, keyword, reply):
-        self.chat_id = str(chat_id)  # ensure string
-        self.keyword = keyword
+    def __init__(
+        self,
+        note, reply, note_type,
+        media_id=None, media_access_hash=None, media_file_reference=None
+    ):
+        self.note = note
         self.reply = reply
+        self.note_type = note_type
+        self.media_id = media_id
+        self.media_access_hash = media_access_hash
+        self.media_file_reference = media_file_reference
 
 
-Notes.__table__.create(checkfirst=True)
+notes.__table__.create(checkfirst=True)
 
 
-def get_notes(chat_id):
+def get_notes(keyword):
     try:
-        return SESSION.query(Notes).filter(Notes.chat_id == str(chat_id)).all()
+        return SESSION.query(notes).get(keyword)
+    except:
+        return None
     finally:
         SESSION.close()
 
 
-def add_note(chat_id, keyword, reply):
-    adder = SESSION.query(Notes).get((str(chat_id), keyword))
+def get_all_notes():
+    try:
+        return SESSION.query(notes).all()
+    except:
+        return None
+    finally:
+        SESSION.close()
+
+
+def add_note(keyword, reply, note_type, media_id, media_access_hash, media_file_reference):
+    adder = SESSION.query(notes).get(keyword)
     if adder:
         adder.reply = reply
+        adder.note_type = note_type
+        adder.media_id = media_id
+        adder.media_access_hash = media_access_hash
+        adder.media_file_reference = media_file_reference
     else:
-        adder = Notes(str(chat_id), keyword, reply)
+        adder = notes(keyword, reply, note_type, media_id,
+                      media_access_hash, media_file_reference)
     SESSION.add(adder)
     SESSION.commit()
 
 
-def rm_note(chat_id, keyword):
-    note = SESSION.query(Notes).filter(Notes.chat_id == str(chat_id), Notes.keyword == keyword)
+def remove_note(keyword):
+    note = SESSION.query(notes).filter(notes.note == keyword)
     if note:
         note.delete()
-        SESSION.commit()
-
-def rm_all_notes(chat_id):
-    notes = SESSION.query(Notes).filter(Notes.chat_id == str(chat_id))
-    if notes:
-        notes.delete()
         SESSION.commit()
